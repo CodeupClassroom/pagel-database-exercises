1eff688ea95c506fb8f55299d202a5ca99d6dd28
Misty Garcia <ms.misty.garcia@gmail.com>
2023-03-10 14:51:38 -0600

    groupby lesson

---
+++ groupby_lesson.sql
@@ -0,0 +1,45 @@
+-- GROUP BY -- 
+
+-- 1. SELECT 
+-- 2. FROM
+-- 3. WHERE
+-- 4. GROUP BY [column_name_to_group]
+
+
+-- let's use the chipotle db
+
+
+
+-- find all unique items with chicken
+
+
+-- can group by multiple variables
+-- find all unqiue combinations of items with chicken and their quantity
+
+
+-- AGGREGRATE FUNCTIONS -- 
+
+-- count
+-- how many chicken bowls have been ordered?
+
+
+-- for each chicken item, how many times was it ordered?
+
+
+
+-- min, max
+
+
+
+-- HAVING -- 
+
+-- 1. SELECT 
+-- 2. FROM
+-- 3. WHERE
+-- 4. GROUP BY [column_name_to_group]
+-- 5. HAVING [filter the grouped data]  
+
+-- find all orders items that have been ordered over 100 times
+
+
+
